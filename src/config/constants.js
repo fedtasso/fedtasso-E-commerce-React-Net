fc22ea6eb38f0e2f4c7264be548386542b11f4e9
fedtasso <fedtasso@gmail.com>
2026-02-10 02:47:35 -0300

    Primer commit - Front React listo para conexión con .NET

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_URL_PRODUCTS = 'https://ecommerce-backend-spring-66th.onrender.com/api/products';
-export const API_URL_USERS = "https://ecommerce-backend-spring-66th.onrender.com/api/users";
-export const API_URL_RESGISTER = "https://ecommerce-backend-spring-66th.onrender.com/api/users/register"
-export const API_URL_LOGIN = "https://ecommerce-backend-spring-66th.onrender.com/api/users/login"
+export const API_URL_PRODUCTS = 'https://https://ecommerce-backend-net.onrender.com/api/products';
+export const API_URL_USERS = "https://https://ecommerce-backend-net.onrender.com/api/users";
+export const API_URL_RESGISTER = "https://https://ecommerce-backend-net.onrender.com/api/users/register"
+export const API_URL_LOGIN = "https://https://ecommerce-backend-net.onrender.com/api/users/login"
